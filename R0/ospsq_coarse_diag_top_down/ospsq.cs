ddb1b28faa7325162b48b1b360fd491a89ab30c9
Chiranjeb Mondal <>
2021-01-29 14:20:20 -0800

    ospsq diag coarse top down

--- R0/ospsq_coarse_diag_top_down/ospsq.cs
+++ R0/ospsq_coarse_diag_top_down/ospsq.cs
@@ -1,7 +1,6 @@
-base_prog = "ospsqmax";
 data_type="float";
-system = base_prog + "_" + data_type;
-prog = ReadAlphabets(system + ".ab");
+system = "ospsqmax_float";
+prog = ReadAlphabets("ospsq_float.ab");
 
 NormalizeReduction(prog, system, "FTable");
 AShow(prog, system);
@@ -10,13 +9,13 @@ Normalize(prog);
 AShow(prog, system);
 
 
-
 setMemoryMap(prog,    system, "NR_FTable",   "FTable",   "(i1,j1,i2,j2->i1, j1, i2, j2)");
 setSpaceTimeMap(prog, system, "FTable",      "(i1,j1,i2,j2       ->  j1-i1,   i1,  j1,    i2,       i2,    j2)");
 setSpaceTimeMap(prog, system, "NR_FTable",   "(i1,j1,i2,j2,k1,k2 ->  j1-i1,   i1,  k1,    i2,       k2,    j2)", 
                                              "(i1,j1,i2,j2       ->  j1-i1,   i1,  i1-1,  i2,       i2-1,  j2)");
 setParallel(prog, system, "", "1");
-outDir = "./src"";
+outDir = "src" ;
+
 generateScheduledCode(prog, system, outDir);
 generateWrapper(prog, system, outDir);
 generateMakefile(prog, system, outDir);
