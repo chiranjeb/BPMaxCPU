a6463e68affdc396f5071280796bf700685da883
Chiranjeb Mondal <chiranjeb.mondal@gmail.com>
2022-01-23 21:42:01 -0800

    2D to 4D transformation APIs

--- bpmax_register_tile/src/transform_2D_to_4D_like_B_for_register_tile.c
+++ bpmax_register_tile/src/transform_2D_to_4D_like_B_for_register_tile.c
@@ -9,108 +9,9 @@
 #include <string.h>
 #include <limits.h>
 #include <float.h>
-#include <omp.h>
 #include <immintrin.h>
-#include <malloc.h>
 
-#include "external_functions.h"
-
-// Common Macros
-#define max(x, y)   ((x)>(y) ? (x) : (y))
-#define MAX(x, y)	((x)>(y) ? (x) : (y))
-#define min(x, y)   ((x)>(y) ? (y) : (x))
-#define MIN(x, y)	((x)>(y) ? (y) : (x))
-#define CEILD(n,d)  (int)ceil(((double)(n))/((double)(d)))
-#define ceild(n,d)  (int)ceil(((double)(n))/((double)(d)))
-#define FLOORD(n,d) (int)floor(((double)(n))/((double)(d)))
-#define floord(n,d) (int)floor(((double)(n))/((double)(d)))
-#define CDIV(x,y)    CEILD((x),(y))
-#define div(x,y)    CDIV((x),(y))
-#define FDIV(x,y)    FLOORD((x),(y))
-#define LB_SHIFT(b,s)  ((int)ceild(b,s) * s)
-#define MOD(i,j)   ((i)%(j))
-#define mallocCheck(v,s,d) if ((v) == NULL) { printf("Failed to allocate memory for %s : size=%lu\n", "sizeof(d)*(s)", sizeof(d)*(s)); exit(-1); }
-// Reduction Operators
-#define RADD(x,y)    ((x)+=(y))
-#define RMUL(x,y)    ((x)*=(y))
-#define RMAX(x,y)    ((x)=MAX((x),(y)))
-#define RMIN(x,y)    ((x)=MIN((x),(y)))
-
-// Common functions for min and max
-//functions for integer max
-inline int __max_int(int x, int y){
-	return ((x)>(y) ? (x) : (y));
-}
-
-inline short __max_short(short x, short y){
-	return ((x)>(y) ? (x) : (y));
-}
-
-inline long __max_long(long x, long y){
-	return ((x)>(y) ? (x) : (y));
-}
-
-inline unsigned int __max_unsigned_int(unsigned int x, unsigned int y){
-	return ((x)>(y) ? (x) : (y));
-}
-
-inline unsigned short __max_unsigned_short(unsigned short x, unsigned short y){
-	return ((x)>(y) ? (x) : (y));
-}
-
-//function for float max
-inline float __max_float(float x, float y){
-	return ((x)>(y) ? (x) : (y));
-}
-
-//function for double max
-inline double __max_double(double x, double y){
-	return ((x)>(y) ? (x) : (y));
-}
-
-//function for integer min
-inline int __min_int(int x, int y){
-	return ((x)>(y) ? (y) : (x));
-}
-
-inline short __min_short(short x, short y){
-	return ((x)>(y) ? (y) : (x));
-}
-
-inline long __min_long(long x, long y){
-	return ((x)>(y) ? (y) : (x));
-}
-
-inline unsigned int __min_unsigned_int(unsigned int x, unsigned int y){
-	return ((x)>(y) ? (y) : (x));
-}
-
-inline unsigned short __min_unsigned_short(unsigned short x, unsigned short y){
-	return ((x)>(y) ? (y) : (x));
-}
-
-inline unsigned long __min_unsigned_long(unsigned long x, unsigned long y){
-	return ((x)>(y) ? (y) : (x));
-}
-
-inline float __min_float(float x, float y){
-	return ((x)>(y) ? (y) : (x));
-}
-
-inline double __min_double(double x, double y){
-	return ((x)>(y) ? (y) : (x));
-}
-
-
-
-
-
-
-//Memory Macros
-#define S2(i,j) S2[i][j]
-#define S2_B(i3,j3) S2_B[i3][j3]
-
-void transform_2D_to_4D_like_B_for_register_tile(long M, long N, long N_sec, long N_tile, long MR, long NR, long I2, long J2, float** S2, float** S2_B){
+void transform_2D_to_4D_like_B_for_register_tile(long M, long N, long N_sec, long N_tile, long MR, long NR, long I2, long J2, float** C, float** B){
 	///Parameter checking
 	if (!((M >= 16 && N >= 96 && N_sec >= 1 && N_tile >= 96 && MR >= 1 && NR >= 1 && I2 >= 0 && J2 >= I2 && N_sec >= J2+1))) {
 		printf("The value of parameters are not valid.\n");
@@ -118,43 +19,20 @@ void transform_2D_to_4D_like_B_for_register_tile(long M, long N, long N_sec, lon
 	}
 	//Memory Allocation
 	
-	#define S0(i3,j3) S2_B(i3,j3) = 0
-	{
-		//Domain
-		//{i3,j3|M>=16 && N>=96 && N_sec>=1 && N_tile>=96 && MR>=1 && NR>=1 && I2>=0 && J2>=I2 && N_sec>=J2+1 && i3>=0 && N_tile>=i3+1 && j3>=0 && N_tile>=j3+1}
-		int c1,c2;
-		for(c1=0;c1 <= N_tile-1;c1+=1)
-		 {
-		 	for(c2=0;c2 <= N_tile-1;c2+=1)
-		 	 {
-		 	 	S0((c1),(c2));
-		 	 }
-		 }
-	}
-	#undef S0
+    float *Pack = &B[0][0];
+	long i_offset = I2 * (N_tile-1);
+    long j_offset = J2 * (N_tile-1) ;
+    for( int jj = 0; jj < N_tile-1; jj+= NR)
+    {
+      for ( int ii = 1; ii < N_tile ; ii++ )
+      {
+          const float *Cptr = &C[i_offset+ii][j_offset+jj];
+          for (int jjj = 0; jjj < NR; jjj++)
+          {
+             *Pack++ = *Cptr++;
+          }
+      }
+   }
 	
 	//Memory Free
 }
-
-//Memory Macros
-#undef S2
-#undef S2_B
-
-
-//Common Macro undefs
-#undef max
-#undef MAX
-#undef min
-#undef MIN
-#undef CEILD
-#undef ceild
-#undef FLOORD
-#undef floord
-#undef CDIV
-#undef FDIV
-#undef LB_SHIFT
-#undef MOD
-#undef RADD
-#undef RMUL
-#undef RMAX
-#undef RMIN
